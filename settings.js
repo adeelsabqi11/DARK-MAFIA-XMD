5d66bd071936c80662bd178be82b32a633b0942f
MAFIA ADEEL <adeelsabqi11@gmail.com>
2026-01-11 22:03:50 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   prefixes: ['.', '!', '/', '#'], // Multiple prefix support you can add one or more
-  packname: 'MEGA-MD',
-  author: '‎GlobalTechInfo',
+  packname: 'DARK-MAFIA-XMD',
+  author: '‎adeelsabqi11',
   timeZone: 'Asia/Karachi',
-  botName: "MEGA-MD", // your bot name
-  botOwner: 'Qasim Ali', // set your name
-  ownerNumber: '923051391007', // Set your number here without + symbol, just add country code & number without any space
+  botName: "DARK-MAFIA-MD", // your bot name
+  botOwner: '𝐌ᴀғɪᴀ 𝐀ᴅᴇᴇʟ', // set your name
+  ownerNumber: '923348585489', // Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20,
@@ -13,8 +13,8 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "5.1.0",
-  updateZipUrl: "https://github.com/GlobalTechInfo/MEGA-MD/archive/refs/heads/main.zip",
-  channelLink: "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07",
+  updateZipUrl: "https://github.com/adeelsabqi11/DARK-MAFIA-XMD/archive/refs/heads/main.zip",
+  channelLink: "https://whatsapp.com/channel/0029VbCDC5M3wtbG50hWK83w",
   ytch: "GlobalTechInfo"
 };
 
